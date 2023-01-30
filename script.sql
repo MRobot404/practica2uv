f5a5fff7c722a02b64eae7d533196275c93ae4e4
Oscar Escobar <qomc16@gmail.com>
2023-01-30 17:32:54 -0600

    Script Inicial

---
+++ script.sql
@@ -0,0 +1,13 @@
+---Creacion de tablas
+CREATE TABLE PERSON(
+USERNAME VARCHAR(30),
+PASSWORD VARCHAR(30)
+);
+
+--Insercion de datos
+insert into person values('test','test');
+
+
+---Seleccion
+Select * from PERSON;
+
